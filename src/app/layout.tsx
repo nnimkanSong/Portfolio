72257cd47123476491e13fc901b913c331341284
nnimkanSong <nnimkanc@student.42bangkok.com>
2025-02-26 02:12:09 +0700

    build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { ReactNode } from "react";
-import Head from "next/head"; // Import Head from Next.js
+import Head from "next/head";
 
 export const metadata = {
   title: "Portfolio",
@@ -13,14 +13,14 @@ interface RootLayoutProps {
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
-    <>
+    <html lang="en">
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{metadata.title}</title>
       </Head>
       <body>{children}</body>
-    </>
+    </html>
   );
 };
 
