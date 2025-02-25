a9df174ce76099e7ecefa257d074754ae00840e4
nnimkanSong <nnimkanc@student.42bangkok.com>
2025-02-26 01:33:03 +0700

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,12 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/profile.ico" type="image/x-icon" />
-        <link rel="shortcut icon" href="/profile.ico" type="image/x-icon" />
-      </head>
       <body>{children}</body>
     </html>
-    
   );
 }
