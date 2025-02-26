a28619de2ffed6d5b477690c33178b3376b7e5f2
nnimkanSong <nnimkanc@student.42bangkok.com>
2025-02-26 21:33:34 +0700

    change-favicon

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // ช่วยให้สามารถโหลดไฟล์ static ได้ง่ายขึ้น
+  images: {
+    unoptimized: true, // ป้องกัน Next.js แปลงไฟล์ภาพ
+  },
 };
 
 export default nextConfig;
